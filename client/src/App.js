7eb723b18d010180b383176e3a4df169eb68ce85
soccermind <mty2yyz@gmail.com>
2021-01-16 17:21:42 -0500

    Added RecipeCard and Favourites components, need styling work

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import useApplicationData from "./hooks/useApplicationData"
 import {Alert} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import RecipeCard from './components/RecipeCard';
+import Favourites from './components/Favourites'
 
 const App = () => {
   const {
@@ -11,23 +13,10 @@ const App = () => {
   } = useApplicationData();
     const userList = state.users.map((user) => (<li key={user.id} > {user.first_name} {user.last_name} {user.email} </li>
 ));
-return (<div className="App" >
-  <h1> Users </h1>
+return (
+<div className="App" >
 
-  <ul> {userList} </ul>
-  <Alert variant="danger">
-  <Alert.Heading>Hey, nice to see you</Alert.Heading>
-  <p>
-    Aww yeah, you successfully read this important alert message. This example
-    text is going to run a bit longer so that you can see how spacing within an
-    alert works with this kind of content.
-  </p>
-  <hr />
-  <p className="mb-0">
-    Whenever you need to, be sure to use margin utilities to keep things nice
-    and tidy.
-  </p>
-</Alert>
+<Favourites />
 </div >
 );
 };
