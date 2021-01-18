f5369926c56a7d530c5151c7957c4d39bc149d44
m-vojjala <mounikavojjala@gmail.com>
2021-01-18 15:47:40 -0500

    api called from client

--- client/src/components/Favourites.js
+++ client/src/components/Favourites.js
@@ -1,23 +1,28 @@
-import React from "react";
-import CardGroup from 'react-bootstrap/CardGroup'
+import {useEffect, useState} from "react";
+import axios from "axios";
+import CardGroup from 'react-bootstrap/CardGroup';
+import response from "../responseData.json";
 
 import RecipeCard from './RecipeCard'
 import "./Favourites.scss";
 
-export default function Favourites (props) {
-  
-  
+export default function Favourites () {
+
+  const [favs,setFavs] = useState([]);
+
+  useEffect(() => {
+    setFavs(response);
+}, []);
+
+const favourites = favs.map((recipe)=> <RecipeCard title={recipe.title} image={recipe.image}/>)
+
   return (
     <section>
       <h1>Favourites</h1>
       <CardGroup>
-        <RecipeCard />
-        <RecipeCard />
-        <RecipeCard />
-        <RecipeCard />
-        <RecipeCard />
-        <RecipeCard />
+        {favourites}
       </CardGroup>
     </section>
   );
 }
+
--- client/src/components/RecipeCard.js
+++ client/src/components/RecipeCard.js
@@ -9,12 +9,11 @@ export default function RecipeCard (props) {
   
   return (
     <Card>
-      <Card.Img variant="top" src="images/recipe_sample.png" />
+      <Card.Img variant="top" src={props.image} />
       <Card.Body>
-        <Card.Title>Recipe Title</Card.Title>
+        <Card.Title>{props.title}</Card.Title>
         <Card.Text>
-          Some quick example text to build on the recipe title and make up the bulk of
-          the card's content.
+         {props.description}
         </Card.Text>
         <div class="card-bottom">
           <Button variant="primary">View Recipe</Button>
