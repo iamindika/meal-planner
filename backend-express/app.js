d9d1fd81426be9fac7f99d3be0c4677bb324b218
soccermind <mty2yyz@gmail.com>
2021-01-19 15:25:27 -0500

    Fixed /recipes bug

--- backend-express/app.js
+++ backend-express/app.js
@@ -9,7 +9,7 @@ const dbRecipeHelpers = require('./db/helpers/dbRecipeHelpers')(db);
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const recipesRouter = require('./routes/recipes');
+const localRecipesRouter = require('./routes/localRecipes');
 
 const app = express();
 
@@ -22,6 +22,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/api/posts', usersRouter(dbHelpers));
 app.use('/api/users', usersRouter(dbHelpers));
-app.use('/recipes', recipesRouter(dbRecipeHelpers));
+app.use('/recipes', localRecipesRouter(dbRecipeHelpers));
 
 module.exports = app;
--- backend-express/routes/recipes.js
+++ backend-express/routes/recipes.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const router = express.Router();
-// const response = require("../responseData.json")
+// const express = require('express');
+// const router = express.Router();
+// // const response = require("../responseData.json")
 
-module.exports = (db) => {
+// module.exports = (db) => {
 
-  router.get("/",(req,res)=>{
-    res.json(response);
-  })
-  return router;
-}
+//   router.get("/",(req,res)=>{
+//     res.json(response);
+//   })
+//   return router;
+// }
