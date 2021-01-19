7fdddd680293189c0c9ab71d233f2e010ff53db8
soccermind <mty2yyz@gmail.com>
2021-01-19 10:26:46 -0500

    Moved local recipes logic to new component localRecipes.js

---
+++ client/src/components/LocalRecipes.js
@@ -0,0 +1,25 @@
+import React from "react";
+import CardGroup from 'react-bootstrap/CardGroup'
+
+import LocalRecipeCard from './LocalRecipeCard'
+import "./Favourites.scss";
+
+import useLocalRecipesData from "../hooks/useLocalRecipesData";
+
+export default function LocalRecipes (props) {
+  const { state, dispatch } = useLocalRecipesData();
+  const recipeList = state.recipes.map((recipe) => (<LocalRecipeCard key={recipe.id} id={recipe.id} name={recipe.name} image={recipe.image} instructions={recipe.instructions}>  {recipe.name} </LocalRecipeCard>));
+
+  
+  return (
+    <section>
+      <h1> Recipes </h1>
+      {/* <ul> {recipeList} </ul> */}
+      <CardGroup>
+
+        {recipeList}
+
+      </CardGroup>
+    </section>
+  );
+}
