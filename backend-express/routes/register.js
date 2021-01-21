7bb36acdc7869f15a90d86d68aed73a04815f0fa
Indika Kolamba Tantrege <Indika@Mac-mini.local>
2021-01-21 05:43:25 -0500

    Install bcrypt and hash passwords on user registration

--- backend-express/routes/register.js
+++ backend-express/routes/register.js
@@ -1,26 +1,42 @@
 const express = require('express');
 const router = express.Router();
-
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 module.exports = ({addUser,getUserByEmail}) => {
   
-  router.post("/",(req,res)=>{
+  router.post("/", (req,res) => {
     const {fName, lName, email, password} = req.body;
-     getUserByEmail(email)
-     .then((user)=> {
-       if (!user) {
-        addUser(fName,lName,email,password)
-           .then(userId=>  {
-          //  console.log(userId.id)
-          console.log(req.session)
-            req.session['user_id'] = userId.id;
-          console.log(req.session)
-        });
-       }else{
-           res.sendStatus(400);
+    
+    //Validation
+    if (!fName || !lName || !email || !password) {
+      console.log('Validation Failed!');
+      return res.status(400).json({ msg: 'Please enter all fields'})
+    }
+
+    getUserByEmail(email)
+      .then((user)=> {
+        if (user) {
+          return res.status(400).json({ msg: 'User already exists! Please login.' });
+        } 
+
+        //Hash password
+        bcrypt.hash(password, saltRounds, function(err, hash) {
+          if (err) {
+            throw err;
           }
-       });
-    })
+          addUser(fName, lName, email, hash)
+            .then(user =>  {
+              res.send({
+                id: user.id,
+                firstName: user.first_name,
+                lastName: user.last_name,
+                email: user.email
+              })
+            });
+        });
+      });
+  })
   return router;
 }
 
