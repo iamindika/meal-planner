d21fec6dd6e784d462c0ebe4401d2cfdc448499e
soccermind <mty2yyz@gmail.com>
2021-01-21 10:50:17 -0500

    Added favourite field to user_recipe and ON DELETE CASCADE to foreign keys on all bridge tables

--- backend-express/db/schema/07_user_diets.sql
+++ backend-express/db/schema/07_user_diets.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS user_diets CASCADE;
 CREATE TABLE user_diets(
     id SERIAL PRIMARY KEY NOT NULL,
-    user_id INTEGER REFERENCES users(id),
-    diet_id INTEGER REFERENCES diets(id)
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    diet_id INTEGER REFERENCES diets(id) ON DELETE CASCADE
 );
