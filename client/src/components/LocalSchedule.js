533e00e0c1d8e494e21d33e3c931c5c1db3875b0
soccermind <mty2yyz@gmail.com>
2021-01-22 10:47:42 -0500

    Added responsive table to schedule, no configuration yet

--- client/src/components/LocalSchedule.js
+++ client/src/components/LocalSchedule.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Table } from "react-bootstrap";
 import CardGroup from 'react-bootstrap/CardGroup'
 import { useEffect, useState } from "react";
 import axios from "axios";
@@ -11,6 +12,7 @@ import "./Favourites.scss";
 export default function LocalSchedule (props) {
   const [userRecipes, setUserRecipes] = useState([]);
   const id = 1 // hard coded for now..
+  const weekdays = [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ];
   useEffect(() => {
     axios({
       method: 'GET',
@@ -29,8 +31,47 @@ export default function LocalSchedule (props) {
 
   return (
     <section>
-      <h1> User Recipes </h1>
-      <ul> {JSON.stringify(userRecipes)} </ul>
+      <h1> Schedule </h1>
+      {/* <ul> {JSON.stringify(userRecipes)} </ul> */}
+      
+    <Table responsive>
+      <thead>
+        <tr>
+          <th>#</th>
+          {weekdays.map((weekday, index) => (
+            <th key={index}>{weekday}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Breakfast</td>
+          {/* {Array.from({ length: 7 }).map((_, index) => (
+            <td key={index}>Table cell {index}</td>
+          ))} */}
+          <td>eggs</td>
+          <td>eggs and bacon</td>
+          <td>pancakes</td>
+          <td>breakfast poutine</td>
+          <td>cereal</td>
+          <td>eggs</td>
+          <td>English Breakfast</td>
+        </tr>
+        <tr>
+          <td>Lunch</td>
+          {Array.from({ length: 7 }).map((_, index) => (
+            <td key={index}>Table cell {index}</td>
+          ))}
+        </tr>
+        <tr>
+          <td>Dinner</td>
+          {Array.from({ length: 7 }).map((_, index) => (
+            <td key={index}>Table cell {index}</td>
+          ))}
+        </tr>
+      </tbody>
+    </Table>
+
       {/* <CardGroup>
 
         {myRecipes}
