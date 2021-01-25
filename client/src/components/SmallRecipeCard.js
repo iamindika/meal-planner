8a6b26498b2931d5d86b59e74ced9d4af5afeb45
soccermind <mty2yyz@gmail.com>
2021-01-24 19:49:16 -0500

    Added functionality to add a recipe to the schedule

--- client/src/components/SmallRecipeCard.js
+++ client/src/components/SmallRecipeCard.js
@@ -7,7 +7,26 @@ import { useState } from "react";
 import "./SmallRecipeCard.scss";
 
 export default function SmallRecipeCard (props) {
-  const [removed, setRemoved ] = useState([props.time_slot, props.day])
+  const [showInstructions, setShowInstructions] = useState(true);
+  console.log("SmallRecipeCard props:", props);
+  
+  const [removed, setRemoved ] = useState({ 
+    time_slot: props.time_slot, 
+    day: props.day, 
+    name: props.name,
+    recipeId: props.recipeId,
+    userId: props.userId
+  })
+  // const ingredientsWithAmount= props.ingredients.map((ingredient)=>{
+  //   return<div><p>
+  //    {ingredient.name} - {ingredient.quantity}: {ingredient.unit}</p></div> 
+  // })
+  const ingredientsWithAmount=[]
+
+  function handleClick(){
+    setShowInstructions(!showInstructions)
+  }
+
   function handleClickRemove(event){
     event.preventDefault();
     // setRemoved(prev => props.recipeId);
@@ -35,12 +54,18 @@ export default function SmallRecipeCard (props) {
       <Card.Img variant="top" src={props.image} />
       <Card.Body>
         <Card.Title>{props.name}</Card.Title>
+        {showInstructions &&
+        <div>
+        <Card.Text>
+          {ingredientsWithAmount}
+        </Card.Text>
         <Card.Text>
           {props.instructions}
         </Card.Text>
+        </div>}
         <div class="card-bottom">
           <div class="button-container">
-            <Button variant="primary">View Recipe</Button>
+            <Button variant="primary" onClick={handleClick}>View Recipe</Button>
           </div>
           <div class="minus-circle-container">
             <button type="submit" onClick={handleClickRemove} style={{ border: "none",backgroundColor: "Transparent"}}>
