bac5d577d5cb95d98ebb3cd3422c08be00a5210b
Indika Kolamba Tantrege <Indika@Mac-mini.local>
2021-01-27 08:44:17 -0500

    Add protected routes

--- client/src/App.js
+++ client/src/App.js
@@ -10,28 +10,38 @@ import Search from "./components/Search"
 import LocalRecipes from "./components/LocalRecipes"
 import LocalSchedule  from "./components/LocalSchedule"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import ViewProfile from './components/ViewProfile';
-
+import PrivateRoute from './components/PrivateRoute';
 
 const App = () => {
   return(
- <div className="App">
-   <Router>
-  <Heading />
-  <Switch>
+    <div className="App">
+      <Router>
+        <Heading />
+        <Switch>
           <Route path="/" exact component={Login} />
-          <Route path="/schedule" component={LocalSchedule} />
-          <Route path="/new/profile" component={Profile} />
-          <Route path="/search" component={Search} />
-          <Route path="/favorites"  component={Favourites} />
           <Route path="/register" component={Register} />
-          <Route path="/new" component={RecipeForm} />
-          <Route path="/recipes" component={LocalRecipes} />
-          <Route path="/profile" component={ViewProfile} />
-          </Switch>
-          </Router>
-          
-</div >
-  )};
+          <PrivateRoute path="/schedule" >
+            <LocalSchedule />
+          </PrivateRoute>
+          <PrivateRoute path="/profile" >
+            <Profile />
+          </PrivateRoute>
+          <PrivateRoute path="/search" >
+            <Search />
+          </PrivateRoute>
+          <PrivateRoute path="/favorites" >
+            <Favourites />
+          </PrivateRoute>
+          <PrivateRoute path="/new" >
+            <RecipeForm />
+          </PrivateRoute>
+          <PrivateRoute path="/recipes" >
+            <LocalRecipes />
+          </PrivateRoute>
+        </Switch>
+      </Router>
+    </div >
+  )
+};
 
 export default App;
